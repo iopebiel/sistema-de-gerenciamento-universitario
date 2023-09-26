8d244f2e1f0e07cee6e780a2208ad45976ae065c
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-09-26 00:20:37 -0300

    Tela de início, login e cadastro com infos básicas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import sqlite3
 
 from flask import Flask, render_template, request, session
@@ -6,19 +7,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = "chave_secreta"
 
-conexao = sqlite3.connect('usuarios.db')
-cursor = conexao.cursor()
+def criar_tabela_alunos():
+    conexao = sqlite3.connect('usuarios.db')
+    cursor = conexao.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS usuarios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT,
-        senha TEXT
-    )
-''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS alunos (
+            turma TEXT PRIMARY KEY,
+            nome TEXT,
+            email TEXT,
+            senha TEXT,
+            curso TEXT,
+            semestre INTEGER,
+            unidade TEXT,
+            numeroprontuario TEXT
+        )
+    ''')
 
-conexao.commit()
-conexao.close()
+    conexao.commit()
+    conexao.close()
 
 
 @app.route('/')
@@ -26,42 +33,107 @@ def index():
     return render_template('cadastro.html')
 
 
-@app.route('/cadastro', methods=['POST'])
+@app.route('/cadastro', methods=['GET','POST'])
 def cadastrar():
-    email = request.form['email']
-    senha = request.form['senha']
-    senha_hash = generate_password_hash(senha, method='sha256')
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        nome = request.form['nome']
+        curso = request.form['curso']
+        
+        # Converta o valor do semestre para um número inteiro
+        semestre = int(request.form['semestre'])
+        
+        # Calcule a turma como a junção de curso e semestre
+        turma = curso + str(semestre)
 
-    conexao = sqlite3.connect('usuarios.db')
-    cursor = conexao.cursor()
+        unidade = request.form['unidade']
+         # Obtenha o número de prontuário do formulário
+        numeroprontuario = request.form['numeroprontuario']
 
-    cursor.execute('INSERT INTO usuarios (email, senha) VALUES (?, ?)', (email, senha_hash))
+        # Valide o número de prontuário (deve conter exatamente 7 dígitos)
+        if not re.match(r'^\d{7}$', numeroprontuario):
+            return "Número de prontuário inválido. Deve conter exatamente 7 dígitos."
 
-    conexao.commit()
-    conexao.close()
+        senha_hash = generate_password_hash(senha, method='sha256')
 
-    return "Cadastro realizado com sucesso!"
+        conexao = sqlite3.connect('usuarios.db')
+        cursor = conexao.cursor()
+        
+        try:
+            # Verifique se o email já existe no banco de dados
+            cursor.execute('SELECT email FROM alunos WHERE email = ?', (email,))
+            usuario_existente = cursor.fetchone()
+
+            if usuario_existente:
+                return "Este email já está cadastrado. Por favor, use outro email."
+
+            senha_hash = generate_password_hash(senha, method='sha256')
+
+            cursor.execute('''
+                        INSERT INTO alunos (nome, email, senha, curso, turma, semestre, unidade, numeroprontuario) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (nome, email, senha_hash, curso, turma, semestre, unidade, numeroprontuario))
+
+            conexao.commit()
+            return "Cadastro realizado com sucesso!"
+        
+        except Exception as e:
+                return f"Erro durante o cadastro: {str(e)}"
+        finally:
+                conexao.close()
+
+    # Se o método for GET, apenas renderize a página de cadastro
+    return render_template('cadastro.html')
+    
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    email = request.form['email']
-    senha = request.form['senha']
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
 
-    conexao = sqlite3.connect('usuarios.db')
-    cursor = conexao.cursor()
+        # Consultar o banco de dados para obter o hash da senha
+        conexao = sqlite3.connect('usuarios.db')
+        cursor = conexao.cursor()
+        cursor.execute('SELECT senha FROM alunos WHERE email = ?', (email,))
+        senha_hash = cursor.fetchone()
 
-    cursor.execute('SELECT * FROM usuarios WHERE email = ?', (email,))
-    usuario = cursor.fetchone()
+        if senha_hash and check_password_hash(senha_hash[0], senha):
+            # Senha correta, autenticar o usuário
+            session['usuario'] = email
+            return render_template('opcoes.html')
+        else:
+            # Credenciais inválidas, mostrar mensagem de erro
+            return "Credenciais inválidas. Tente novamente."
 
-    conexao.close()
+        # Outras rotas e configurações do aplicativo
+        # Processar o formulário de login
+        email = request.form['email']
+        senha = request.form['senha']
 
-    if usuario and check_password_hash(usuario[2], senha):
-        session['usuario'] = usuario[1]
-        return render_template('opcoes.html')
-    else:
-        return "Credenciais inválidas"
+        conexao = sqlite3.connect('usuarios.db')
+        cursor = conexao.cursor()
 
+        cursor.execute('SELECT senha FROM alunos WHERE email = ?', (email,))
+        senha_hash = cursor.fetchone()
+
+        conexao.close()
+
+ 
+        if usuario and check_password_hash(usuario[2], senha):
+        # Credenciais válidas
+            session['usuario'] = senha_hash[1]
+            return render_template('opcoes.html')
+        else:
+        # Credenciais inválidas
+            return "Credenciais inválidas"
+
+    # Se o método for GET, apenas renderize a página de login
+    return render_template('login.html')
+   
 
 if __name__ == '__main__':
+    criar_tabela_alunos()
     app.run(debug=True)
