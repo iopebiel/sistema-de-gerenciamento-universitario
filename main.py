8c9b9e28e07a81577951805c3e845cdf917ee28f
Gabriel Iope <gabriel.r.iope@gmail.com>
2023-10-14 06:53:04 -0300

    Ínicio de implementação de redefinir senha

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import random
+import smtplib
+from email.mime.text import MIMEText
 import re
 import sqlite3
 
-from flask import Flask, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "chave_secreta"
+
+verification_codes = {}
 
 def criar_tabela_alunos():
     conexao = sqlite3.connect('usuarios.db')
@@ -13,14 +18,14 @@ def criar_tabela_alunos():
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS alunos (
-            turma TEXT PRIMARY KEY,
+            numeroprontuario TEXT PRIMARY KEY,
             nome TEXT,
             email TEXT,
             senha TEXT,
             curso TEXT,
             semestre INTEGER,
             unidade TEXT,
-            numeroprontuario TEXT
+            turma TEXT
         )
     ''')
 
@@ -30,7 +35,7 @@ def criar_tabela_alunos():
 
 @app.route('/')
 def index():
-    return render_template('cadastro.html')
+    return render_template('login.html')
 
 
 @app.route('/cadastro', methods=['GET','POST'])
@@ -66,7 +71,7 @@ def cadastrar():
             usuario_existente = cursor.fetchone()
 
             if usuario_existente:
-                return "Este email já está cadastrado. Por favor, use outro email."
+                flash("Este email já está cadastrado.", "danger")
 
             senha_hash = generate_password_hash(senha, method='sha256')
 
@@ -103,15 +108,59 @@ def login():
         if senha_hash and check_password_hash(senha_hash[0], senha):
             # Senha correta, autenticar o usuário
             session['usuario'] = email
-            return render_template('opcoes.html')
+            return redirect ('/inicio')
         else:
             # Credenciais inválidas, mostrar mensagem de erro
-            return "Credenciais inválidas. Tente novamente."
-
+                flash("Credenciais inválidas. Tente novamente.", "danger")
 
     # Se o método for GET, apenas renderize a página de login
     return render_template('login.html')
+
+@app.route('/codigo_email', methods=['POST'])
+def codigo_email():
+    email = request.form.get('email')
+    codigo_verificacao = str(random.randint(100000, 999999))  # Gere o código de verificação
+    verification_codes[email] = codigo_verificacao  # Armazene o código associado ao e-mail
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = 'gabriel.iope@aluno.ifsp.edu.br'
+    smtp_password = 'Biel7920311'
+
+    # Crie a mensagem de e-mail
+    msg = MIMEText(f'Seu código de verificação: {codigo_verificacao}')
+    msg['Subject'] = 'Código de Verificação'
+    msg['From'] = 'gabriel.iope@aluno.ifsp.edu.br'
+    msg['To'] = email
+
+    # Conecte-se ao servidor SMTP e envie o e-mail
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.sendmail('gabriel.iope@aluno.ifsp.edu.br', [email], msg.as_string())
+    server.quit()
+
    
+@app.route('/redefinir_senha', methods=['GET','POST'])
+def redefinir_senha():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        nova_senha = request.form.get('nova-senha')
+        
+        conexao = sqlite3.connect('usuarios.db')
+        cursor = conexao.cursor()
+        
+        cursor.execute('UPDATE usuarios SET senha = ? WHERE email = ?', (nova_senha, email))
+        conexao.commit()
+        conexao.close()
+
+        # Redirecione o usuário para uma página de sucesso
+        return render_template('login.html')
+        
+    return render_template('redefinir_senha.html')
+
+@app.route('/inicio')
+def inicio():
+    return render_template ('inicio.html')
 
 if __name__ == '__main__':
     criar_tabela_alunos()
